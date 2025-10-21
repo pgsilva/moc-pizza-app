0fd92a59ce927ffd1aed27dda9f08d9ba1f5f010
Paulo Guilherme <pgsilva0698@gmail.com>
2025-10-21 19:03:13 -0300

    fix next

--- moc-pizza/next.config.ts
+++ moc-pizza/next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from "next";
 
 const withPWA = require('next-pwa')({
-  dest: 'public', // Onde o Service Worker será gerado
-  disable: process.env.NODE_ENV === 'development', // Desabilita PWA em modo de desenvolvimento para facilitar o debug
-  register: true, // Registra o Service Worker
-  skipWaiting: true, // Força a nova versão do Service Worker a assumir o controle mais rápido
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['via.placeholder.com'],
   },
+  // outras opções do Next.js
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
